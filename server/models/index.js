dc75775ad59e89b64d619a0ee4981eed250fe9eb
Darksller <darksller.sss@gmail.com>
2023-12-25 01:21:32 +0300

    .

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,10 @@
 import { Sequelize } from 'sequelize'
 import user from './user.mjs'
+import mysql2 from 'mysql2'
 
 const sequelize = new Sequelize('sql11672529', 'sql11672529', 'EjJVpejQCl', {
 	dialect: 'mysql',
+	dialectModule: mysql2,
 	host: 'sql11.freemysqlhosting.net',
 	port: 3306,
 	logging: false,
