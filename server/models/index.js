3e70015c9de3384cc58ab427f0d871f5908b6773
Darksller <darksller.sss@gmail.com>
2023-12-25 01:10:59 +0300

    .

--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,7 @@ import user from './user.mjs'
 
 const sequelize = new Sequelize('sql11672529', 'sql11672529', 'EjJVpejQCl', {
 	dialect: 'mysql',
+	dialectModule: require('mysql2'),
 	host: 'sql11.freemysqlhosting.net',
 	port: 3306,
 	logging: false,
